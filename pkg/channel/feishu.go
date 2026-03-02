a55851be4e93a13c2147a2f17460855fb8bc3a97
yockii <xuyuqi@gmail.com>
2026-03-02 23:54:26 +0800

    feat(feishu): 添加机器人openID获取和群聊消息处理功能
    添加获取机器人openID的逻辑，用于识别@机器人消息
    实现群聊消息历史记录功能，保留最近10条消息
    处理@机器人消息时返回历史消息记录

--- pkg/channel/feishu.go
+++ pkg/channel/feishu.go
@@ -3,12 +3,18 @@ package channel
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log/slog"
+	"net/http"
+	"regexp"
+	"strings"
 	"time"
 
 	lark "github.com/larksuite/oapi-sdk-go/v3"
 	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 	"github.com/larksuite/oapi-sdk-go/v3/event/dispatcher"
+	larkauth "github.com/larksuite/oapi-sdk-go/v3/service/auth/v3"
 	larkim "github.com/larksuite/oapi-sdk-go/v3/service/im/v1"
 	larkws "github.com/larksuite/oapi-sdk-go/v3/ws"
 	"github.com/yockii/yoclaw/pkg/bus"
@@ -16,11 +22,12 @@ import (
 
 func NewFeishuChannel(name, appID, appSecret string) *FeishuChannel {
 	c := &FeishuChannel{
-		name:        name,
-		appID:       appID,
-		appSecret:   appSecret,
-		stopCh:      make(chan struct{}, 1),
-		reconnectCh: make(chan struct{}, 1),
+		name:         name,
+		appID:        appID,
+		appSecret:    appSecret,
+		stopCh:       make(chan struct{}, 1),
+		reconnectCh:  make(chan struct{}, 1),
+		groupHistory: make(map[string][]string),
 	}
 
 	eventHandler := dispatcher.NewEventDispatcher("", "").
@@ -53,11 +60,90 @@ type FeishuChannel struct {
 	restClient  *lark.Client
 	stopCh      chan struct{}
 	reconnectCh chan struct{}
+
+	groupHistory map[string][]string // 群聊chat_id -> 最近10条消息列表
+
+	openID        string
+	channelStatus int
 }
 
 func (c *FeishuChannel) Start() error {
+	// 获取机器人的openID
+	if err := c.getBotOpenID(); err != nil {
+		return err
+	}
+
 	go c.connectToFeishu()
 	go c.monitor()
+	return nil
+}
+
+func (c *FeishuChannel) getBotOpenID() error {
+	req := larkauth.NewInternalTenantAccessTokenReqBuilder().
+		Body(larkauth.NewInternalTenantAccessTokenReqBodyBuilder().
+			AppId(c.appID).
+			AppSecret(c.appSecret).
+			Build()).
+		Build()
+	resp, err := c.restClient.Auth.V3.TenantAccessToken.Internal(context.Background(), req)
+	if err != nil {
+		return fmt.Errorf("failed to get token: %v", err)
+	}
+	if !resp.Success() {
+		return fmt.Errorf("failed to get token: %v", resp.CodeError)
+	}
+	tat := struct {
+		Code              int    `json:"code"`
+		Msg               string `json:"msg"`
+		Expire            int    `json:"expire"`
+		TenantAccessToken string `json:"tenant_access_token"`
+	}{}
+	if err := json.Unmarshal(resp.RawBody, &tat); err != nil {
+		return fmt.Errorf("failed to unmarshal token: %v", err)
+	}
+	token := tat.TenantAccessToken
+	if token == "" {
+		return fmt.Errorf("tenant_access_token is empty")
+	}
+	// 获取机器人信息
+	httpReq, err := http.NewRequest(http.MethodGet, "https://open.feishu.cn/open-apis/bot/v3/info", nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+	httpReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	httpReq.Header.Set("Content-Type", "application/json")
+	httpResp, err := http.DefaultClient.Do(httpReq)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %v", err)
+	}
+	defer httpResp.Body.Close()
+	if httpResp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to get bot info: %v", httpResp.Status)
+	}
+	bodyBytes, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %v", err)
+	}
+	botInfo := struct {
+		Code int    `json:"code"`
+		Msg  string `json:"msg"`
+		Bot  struct {
+			ActivateStatus int    `json:"activate_status"`
+			AppName        string `json:"app_name"`
+			OpenID         string `json:"open_id"`
+		} `json:"bot"`
+	}{}
+	if err := json.Unmarshal(bodyBytes, &botInfo); err != nil {
+		return fmt.Errorf("failed to unmarshal bot info: %v", err)
+	}
+	c.openID = botInfo.Bot.OpenID
+	if c.openID == "" {
+		return fmt.Errorf("open_id is empty")
+	}
+	c.channelStatus = botInfo.Bot.ActivateStatus
+	if c.channelStatus != 2 {
+		return fmt.Errorf("bot is not activated")
+	}
 	return nil
 }
 
@@ -145,6 +231,70 @@ func (c *FeishuChannel) handleMessage(event *larkim.P2MessageReceiveV1) {
 	content := ""
 	if contentPtr != nil {
 		content = *contentPtr
+	}
+
+	msgTypePtr := event.Event.Message.MessageType
+	msgType := ""
+	if msgTypePtr != nil {
+		msgType = *msgTypePtr
+	}
+
+	chatTypePtr := event.Event.Message.ChatType
+	chatType := ""
+	if chatTypePtr != nil {
+		chatType = *chatTypePtr
+	}
+	if chatType == "p2p" {
+		if msgType == larkim.MsgTypeText {
+			body := struct {
+				Text string `json:"text"`
+			}{}
+			if err := json.Unmarshal([]byte(content), &body); err != nil {
+				slog.Error("Feishu Channel handleMessage error", "err", err)
+				return
+			}
+			content = body.Text
+		}
+	} else {
+		// group
+		if msgType == larkim.MsgTypeText {
+			body := struct {
+				Text string `json:"text"`
+			}{}
+			if err := json.Unmarshal([]byte(content), &body); err != nil {
+				slog.Error("Feishu Channel handleMessage error", "err", err)
+				return
+			}
+			content = body.Text
+			// 去掉所有 @_user_{0-100} 格式的字符串
+			content = regexp.MustCompile(`@_user_[0-9]+`).ReplaceAllString(content, "")
+		} else {
+			return
+		}
+		// 看看是否@机器人
+		methionMe := false
+		if len(event.Event.Message.Mentions) > 0 {
+			for _, mention := range event.Event.Message.Mentions {
+				if mention != nil && mention.Id != nil && mention.Id.OpenId != nil && *mention.Id.OpenId == c.openID {
+					methionMe = true
+					break
+				}
+			}
+		}
+		if methionMe {
+			// 构造消息内容，将历史十条消息拼接起来
+			content = fmt.Sprintf("最近10条消息:\n%s\n当前消息:%s", strings.Join(c.groupHistory[chatID], "\n"), content)
+		} else {
+			// 将消息保留到最近10条
+			if c.groupHistory == nil {
+				c.groupHistory = make(map[string][]string)
+			}
+			c.groupHistory[chatID] = append(c.groupHistory[chatID], content)
+			if len(c.groupHistory[chatID]) > 10 {
+				c.groupHistory[chatID] = c.groupHistory[chatID][1:]
+			}
+			return
+		}
 	}
 
 	bus.Default().PublishInbound(bus.InboundMessage{
